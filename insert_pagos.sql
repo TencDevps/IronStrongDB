b57adece0f6072c1bcec874228c1610f57e0b037
Cedenofofo <fofocedeno8@gmail.com>
2025-12-09 18:53:46 -0600

    Limpieza: eliminar archivos sueltos de la raíz (ya organizados en carpetas)

--- insert_pagos.sql
+++
@@ -1,7 +0,0 @@
-USE IronStrongFitness;
-GO
-----
-INSERT INTO Pagos (IdContrato, Monto, FechaPago)
-SELECT IdContrato, 30, GETDATE()
-FROM Contratos;
-GO
--- tabla_Entrenadores.sql
+++
@@ -1,9 +0,0 @@
-USE IronStrongFitness;
-GO
-
-CREATE TABLE Entrenadores (
-    IdEntrenador INT IDENTITY(1,1) PRIMARY KEY,
-    Nombre NVARCHAR(150) NOT NULL,
-    Especialidad NVARCHAR(100)
-);
-GO
